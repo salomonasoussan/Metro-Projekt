649b8018913050878df4fd71c4b357b059499bb7
salomonsoussan <salomon.a.soussan@gmail.com>
2025-11-16 18:33:10 +0100

    Änderungen 11.11., Normierte Funktion erstellt

--- mainschlomi.py
+++ mainschlomi.py
@@ -5,26 +5,33 @@ import random as rnd
 
 a = 0
 b = 1
+m = 1
 
-x_i = np.random.uniform(a, b)
+x_i = 1
+r = 1
 
-def function_f(y_i, y, u_y):
+def function_f(y_i, my, sigma):
+    return 1/(np.sqrt(2*np.pi)*sigma)*np.exp(-(y_i-my)**2/(2*sigma**2))
+
+
+
+def norm_function(function_f, y_i, y, u_y):
     global sigma
     global my
     sigma = u_y
     my = y
-    f = 1/(np.sqrt(2*np.pi)*sigma)*np.exp(-(y_i-my)**2/(2*sigma**2))
 
-    #Normierungskonstante berechnen
+    integral, err = sp.integrate.quad(function_f, my - 3 * sigma, my + 3 * sigma)
 
-    integral = sp.quad(f, my - 3*sigma, my + 3*sigma)
 
-    norm = 1/integral
-    f_norm = norm * f
+    norm = 1 / integral
+    f_norm = norm * function_f
 
     return f_norm
 
-    return f
 
+while(r >= norm_function(function_f, x_i, 6, 0.5)):
+    r = np.random.uniform(0, m)
+    x_i = np.random.uniform(a, b)
+    print("x_i = ", x_i, "r= ", r)
 
-print(function_f(x_i, 1, 2))
