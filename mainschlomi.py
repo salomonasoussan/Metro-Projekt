145cb553458106f65c0cd5500c17475ac2ca88a2
salomonsoussan <salomon.a.soussan@gmail.com>
2025-11-16 20:00:38 +0100

    Änderungen 11.11., Normierte Funktion erstellt

--- mainschlomi.py
+++ mainschlomi.py
@@ -3,35 +3,46 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import random as rnd
 
-a = 0
-b = 1
-m = 1
+all_x = np.array([3, 6, 9, 12])
+all_y = np.array([6, 12, 15, 18])
+all_u_y = np.array([0.50, 0.8, 2.3, 4.4])
 
-x_i = 1
-r = 1
+def function_f(y_i, mu, sigma):
+    return 1/(np.sqrt(2*np.pi)*sigma)*np.exp(-(y_i-mu)**2/(2*sigma**2))
 
-def function_f(y_i, my, sigma):
-    return 1/(np.sqrt(2*np.pi)*sigma)*np.exp(-(y_i-my)**2/(2*sigma**2))
+def generate_random_y(mu, sigma):
+    a = mu - 3 * sigma
+    b = mu + 3 * sigma
 
-
-
-def norm_function(function_f, y_i, y, u_y):
-    global sigma
-    global my
-    sigma = u_y
-    my = y
-
-    integral, err = sp.integrate.quad(function_f, my - 3 * sigma, my + 3 * sigma)
+    m = 1.0
 
 
-    norm = 1 / integral
-    f_norm = norm * function_f
+    while(True):
+        r = np.random.uniform(0, m)
+        x_i = np.random.uniform(a, b)
+        f_x_i = function_f(x_i, 6, 0.5)
+        if r < f_x_i:
+            return x_i
 
-    return f_norm
+        print(x_i, r, f_x_i)
 
+def koeffizienten_berechnung(all_x, all_y):
+    a_0 = ((np.dot(all_x, all_y)*np.sum(all_x)) - (np.sum(all_y) * np.sum(all_x**2)))/((np.sum(all_x) * np.sum(all_x)) - len(all_y)*np.sum(all_x**2))
+    a_1 = (len(all_y)*np.dot(all_x, all_y) - (np.sum(all_y) * np.sum(all_x)))/(len(all_y)*np.sum(all_x**2) - (np.sum(all_x))**2)
+    coefficients = [a_0, a_1]
 
-while(r >= norm_function(function_f, x_i, 6, 0.5)):
-    r = np.random.uniform(0, m)
-    x_i = np.random.uniform(a, b)
-    print("x_i = ", x_i, "r= ", r)
+    return coefficients
 
+def alle_koeffizienten(all_x, all_y, all_u_y):
+    new_random_y = []
+    coefficients = []
+    coefficients.append(koeffizienten_berechnung(all_x, all_y))
+    for i in range(1, 10):
+        for j in range(len(all_y)):
+            new_random_y.append(generate_random_y(all_y[j], all_u_y[j]))
+
+        coefficients.append(koeffizienten_berechnung(all_x, new_random_y))
+
+    return coefficients
+
+print(alle_koeffizienten(all_x, all_y, all_u_y))
